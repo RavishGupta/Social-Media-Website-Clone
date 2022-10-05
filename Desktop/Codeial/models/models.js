34f256d2942c625146336da9d975e590fe22cea9
RavishGupta <ravishgupta2004.coder@gmail.com>
2022-10-05 11:51:09 +0530

    setup the master branch for using passport js

--- Desktop/Codeial/models/models.js
+++ Desktop/Codeial/models/models.js
@@ -1,22 +1,24 @@
-const mongoose=require('mongoose');
-const { use } = require('../routes');
+const mongoose = require('mongoose');
+
 const userSchema = new mongoose.Schema({
-    email:{
-        type:String,
-        required:true,
-        unique:true
+    email: {
+        type: String,
+        required: true,
+        unique: true
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
     },
-    name:{
-        type:String,
-        required:true 
+    name: {
+        type: String,
+        required: true
     }
-},{
-    timestamps:true
+}, {
+    timestamps: true
 });
 
-const user= mongoose.model('User', userSchema);
-module.exports=user;
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
